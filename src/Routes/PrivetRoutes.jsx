14c1c06e4719fef702074845f15e8e0e7205989b
arifadnan05 <arifahamedadnan@gmail.com>
2024-04-27 01:50:33 +0600

    privet routes added

---
+++ src/Routes/PrivetRoutes.jsx
@@ -0,0 +1,26 @@
+import { useContext } from 'react'
+import { AuthContext } from '../providers/AuthProvider'
+import PropTypes from 'prop-types';
+import { Navigate, useLocation } from 'react-router-dom'
+
+const PrivetRoutes = ({ children }) => {
+    const { user, loading } = useContext(AuthContext);
+    const location = useLocation();
+
+    if (loading) {
+        return <span className="loading loading-bars loading-lg"></span>
+    }
+
+    if (user) {
+        return children;
+    }
+
+
+    return <Navigate state={location.pathname} to='/login'></Navigate>
+}
+
+PrivetRoutes.propTypes = {
+    children: PropTypes.object
+}
+
+export default PrivetRoutes
